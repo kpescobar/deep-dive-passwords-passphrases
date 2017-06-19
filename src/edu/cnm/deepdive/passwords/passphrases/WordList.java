e65d64d72e01fdfdab78d15a0b184ad6ea4c05a0
kpescobar <kellyp.721@live.com>
2017-06-19 08:51:51 -0700

    Added word list method and imported word list file.

--- src/edu/cnm/deepdive/passwords/passphrases/WordList.java
+++ src/edu/cnm/deepdive/passwords/passphrases/WordList.java
@@ -1,12 +1,19 @@
 package edu.cnm.deepdive.passwords.passphrases;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class WordList {
 
   public static final int MINIMUM_WORD_LENGTH = 5;
+
+  public static final String WORD_LIST_FILE = "resources/eff_large_wordlist.txt";
 
   private static final String PROPERTIES_FILE = "resources/text.properties";
 
@@ -23,6 +30,8 @@ public class WordList {
       } else if (phraseLength < MINIMUM_WORD_LENGTH) {
         System.out.println(warningMessage);
       }
+      String[] wordList = getWordList(WORD_LIST_FILE);
+      System.out.println(Arrays.toString(wordList)); //FIXME - Get rid of this debugging.
     } catch (NumberFormatException ex) {
       ex.printStackTrace();
       System.out.println(errorMessage);
@@ -38,8 +47,21 @@ public class WordList {
     }
   }
 
-  private static void loadResources() 
-      throws IOException {
+  public static String[] getWordList(String listPath) 
+      throws IOException { 
+    try (BufferedReader reader 
+        = new BufferedReader(
+            new InputStreamReader(
+                WordList.class.getClassLoader().getResourceAsStream(listPath)))) {
+      ArrayList<String> words = new ArrayList<>();
+      for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+        words.add(line.split("\\s+")[1]);
+      }
+      return words.toArray(new String[]{});
+    }
+   }
+
+  private static void loadResources() throws IOException {
     Properties properties = new Properties();
     try (InputStream input = WordList.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE)) {
       properties.load(input);
