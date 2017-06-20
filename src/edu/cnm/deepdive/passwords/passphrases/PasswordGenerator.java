e4f3e4b2e1c53fd9eee6d00ff38bcaf93fd2e025
kpescobar <kellyp.721@live.com>
2017-06-20 11:03:30 -0700

    Javadoc comments

---
+++ src/edu/cnm/deepdive/passwords/passphrases/PasswordGenerator.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.passwords.passphrases;
+
+/**
+ * Implementation of a simple password generator.
+ * @author Kelly Escobar
+ * @version 1.0
+ */
+public class PasswordGenerator {
+
+  /**
+   * Test rig for generating passwords.
+   * @param args Command-line parameters for password generation options.
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
