0e1976a68165af5db5cd09e3af8f60249cb42317
kpescobar <kellyp.721@live.com>
2017-06-21 10:15:07 -0700

    Removed unused imports.

--- src/edu/cnm/deepdive/passwords/passphrases/WordList.java
+++ src/edu/cnm/deepdive/passwords/passphrases/WordList.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Properties;
 import java.util.Random;
 
