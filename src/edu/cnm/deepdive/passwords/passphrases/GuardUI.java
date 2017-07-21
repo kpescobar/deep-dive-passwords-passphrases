36dfb2b6106398af59dc8cb4756d9305e29c5bbb
kpescobar <kellyp.721@live.com>
2017-07-21 14:26:56 -0600

    Added window and tabs for Password/phrases to Security Guard.

--- src/edu/cnm/deepdive/passwords/passphrases/GuardUI.java
+++ src/edu/cnm/deepdive/passwords/passphrases/GuardUI.java
@@ -6,9 +6,12 @@ package edu.cnm.deepdive.passwords.passphrases;
 import java.util.ResourceBundle;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -20,6 +23,23 @@ public class GuardUI extends Application {
   private static final String STYLE_SHEET = "/styles/application.css";
   private static final String LAYOUT_RESOURCE = "/layouts/Generate.fxml";
   private static final String UI_BUNDLE = "resources/ui";
+  
+  private static final String WINDOW_TITLE_KEY = "window.title";
+  
+  @FXML
+  private VBox passwordOptions;
+  @FXML
+  private VBox passphraseOptions;
+  @FXML
+  private CheckBox upperIncluded;
+  @FXML
+  private CheckBox lowerIncluded;
+  @FXML
+  private CheckBox punctuationIncluded;
+  @FXML
+  private CheckBox ambiguousExcluded;
+  
+  private SecurePasswordGenerator passwordGenerator = new SecurePasswordGenerator();
 
   /**
    * @param args
@@ -36,8 +56,27 @@ public class GuardUI extends Application {
     Scene scene = new Scene(pane, 400, 400);
     scene.getStylesheets().add(getClass().getResource(STYLE_SHEET).toExternalForm());
     primaryStage.setScene(scene);
+    primaryStage.setTitle(uiBundle.getString(WINDOW_TITLE_KEY));
+//    upperIncluded.setSelected(passwordGenerator.isUpperCaseIncluded());
+//    lowerIncluded.setSelected(passwordGenerator.isLowerCaseIncluded());
+//    punctuationIncluded.setSelected(passwordGenerator.isPunctuationIncluded());
+//    ambiguousExcluded.setSelected(passwordGenerator.isAmbiguousExcluded());
     primaryStage.show();
   }
   
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
